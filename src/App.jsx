44ddf356442275162f9e3e2c4b245ec9863b56bf
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-23 22:40:09 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './Components/Header'
+import Home from './Components/Home'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <>
       <Header/>
+      <Home/>
+      <Footer/>
     </>
   )
 }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className='h-[90vh] bg-[#6c9affe1]'>
+        <div className='my-auto mx-0 text-center'>Welcome</div>
+    </div>
+    
+  )
+}
+
+export default Welcome
