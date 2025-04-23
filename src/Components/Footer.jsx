969e9deef4b8ebcf32c5b5e15a8d246f4156a40f
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-23 22:42:36 +0530

    minor changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+
+        <h1 className='tit'>LOT<span className='redd '>HUB</span></h1>
+
+      <div className="info">
+        <li>Welcome to LotBit, your ultimate destination for endless Blog adventures. Explore a vast library of Blogs and dive into a world of entertainment curated just for you. With personalized recommendations and a sleek, user-friendly interface, LotBit makes every viewing experience unforgettable.</li>
+      </div>
+
+      <div className="links">
+        <a href="#">Privacy Policy</a>
+        <a href="#">FAQ</a>
+        <a href="#">About</a>
+        <a href="#">Contact Us</a>
+      </div>
+
+    </footer>
   )
 }
 
