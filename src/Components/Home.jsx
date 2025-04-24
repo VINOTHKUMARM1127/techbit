41044f32546ac6e6e9fa270fda67f980c685a125
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-24 22:56:26 +0530

    minor changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Welcome = () => {
   return (
     <div className='h-[90vh] bg-[#6c9affe1]'>
-        <div className='my-auto mx-0 text-center'>Welcome</div>
+        <div className='pt-[300px] text-center'>Welcome To The Blog World</div>
     </div>
     
   )
