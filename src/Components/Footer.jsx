b3ad147938fdc8f595a9a8e228599f19813ebb43
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-24 22:50:20 +0530

    minor changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className=" text-center py-4">
 
-        <h1 className='tit'>LOT<span className='redd '>HUB</span></h1>
+        <h1 className='text-[1.4em]'>TECH<span className=''>BIT</span></h1>
 
-      <div className="info">
-        <li>Welcome to LotBit, your ultimate destination for endless Blog adventures. Explore a vast library of Blogs and dive into a world of entertainment curated just for you. With personalized recommendations and a sleek, user-friendly interface, LotBit makes every viewing experience unforgettable.</li>
+      <div className="py-2">
+        <div className='w-[60%] mx-auto my-2'>Welcome to TechBit, your ultimate destination for endless Blog adventures. Explore a vast library of Blogs and dive into a world of entertainment curated just for you. With personalized recommendations and a sleek, user-friendly interface, TechBit makes every viewing experience unforgettable.</div>
       </div>
 
-      <div className="links">
-        <a href="#">Privacy Policy</a>
-        <a href="#">FAQ</a>
-        <a href="#">About</a>
-        <a href="#">Contact Us</a>
+      <div className="">
+        <a className='px-4' href="#">Privacy Policy</a>
+        <a className='px-4' href="#">FAQ</a>
+        <a className='px-4' href="#">About</a>
+        <a className='px-4' href="#">Contact Us</a>
       </div>
 
     </footer>
