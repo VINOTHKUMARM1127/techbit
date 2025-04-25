4b2b9c67c1d146ba46c3e3e6d4d4191c0fe4b138
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-25 23:59:25 +0530

    minor changes

--- src/data.js
+++ src/data.js
@@ -61,6 +61,18 @@ const data = [
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
     image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
     author:"Vinoth"
+},{
+    id:10,
+    name:"Poco M2 Pro Custom Rom",
+    Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
+    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    author:"Vinoth"
+},{
+    id:11,
+    name:"Poco M2 Pro Custom Rom",
+    Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
+    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    author:"Vinoth"
 },
 ];
 
