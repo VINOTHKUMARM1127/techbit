81ff6ab73a4187ebdd972022454ac50993e876a3
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-25 23:43:27 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Components/Header'
 import Home from './Components/Home'
 import Footer from './Components/Footer'
-import {Router,Routes,Route} from 'axios'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
