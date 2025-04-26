99013e8cbe3d23f459d80c0a5eb7ec13408e322a
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-26 22:57:05 +0530

    minor changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,10 +4,6 @@ import Display from '../Pages/Display'
 const Welcome = () => {
   return (
     <section>
-      <div className='h-[90vh] bg-[#6c9affe1]'>
-        <div className='pt-[300px] text-center'>Welcome To The Blog World</div>
-      </div>
-
       <Display/>
     </section>
 
