e0973861098e91f33f750f8ea9654cdf9cee6fca
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-26 23:03:56 +0530

    minor changes

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -10,11 +10,13 @@ const Details = () => {
   console.log(num)
 
   return (
-        <div className='text-center my-20'>
+        <div className='text-center my-20 mx-2'>
           <div className='flex justify-center'>
             <img className='w-[300px]' src={num.image} alt={num.name}/>
           </div>
-          <div>{num.name}</div>
+          <div className='text-[1.3em] font-bold mt-3 mb-2'>{num.name}</div>
+          <div className='text-[1.2em] mt-3 mb-2 mx-6'>{num.Description}</div>
+          <div className='text[1.1em] opacity-80'><span className='font-bold'>Author:</span>{num.author}</div>
         </div>
       
     
