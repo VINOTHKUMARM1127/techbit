3f138bd74a1c8ef43baa659623be3406a56a2303
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-27 22:58:53 +0530

    Scrool top:

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import blog  from '../data'
+import Display from './Display';
 
 const Details = () => {
 
@@ -17,6 +18,7 @@ const Details = () => {
           <div className='text-[1.3em] font-bold mt-3 mb-2'>{num.name}</div>
           <div className='text-[1.2em] mt-3 mb-2 mx-6 text-justify'>{num.Description}</div>
           <div className='text[1.1em] opacity-80'><span className='font-bold'>Author:</span>{num.author}</div>
+          <Display />
         </div>
       
     
