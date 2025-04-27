0af48791886fa962e8d9ba00ecc1aa0cb9791154
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-27 22:56:07 +0530

    Scrool top:

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -15,7 +15,7 @@ const Details = () => {
             <img className='w-[300px]' src={num.image} alt={num.name}/>
           </div>
           <div className='text-[1.3em] font-bold mt-3 mb-2'>{num.name}</div>
-          <div className='text-[1.2em] mt-3 mb-2 mx-6'>{num.Description}</div>
+          <div className='text-[1.2em] mt-3 mb-2 mx-6 text-justify'>{num.Description}</div>
           <div className='text[1.1em] opacity-80'><span className='font-bold'>Author:</span>{num.author}</div>
         </div>
       
