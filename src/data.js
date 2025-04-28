c1949b446a25be4a3e13a3d393fc950c0af5b9a1
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-04-28 21:14:49 +0530

    Add Image Data

--- src/data.js
+++ src/data.js
@@ -1,77 +1,79 @@
+import pic from './assets/image.png'
+
 const data = [
     {
     id:1,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },
 {
     id:2,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },
 {
     id:3,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },
 {
     id:4,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },
 {
     id:5,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },
 {
     id:6,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },
 {
     id:7,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },
 {
     id:8,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },
 {
     id:9,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },{
     id:10,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },{
     id:11,
     name:"Poco M2 Pro Custom Rom",
     Description:`The POCO M2 Pro is a 6.67" phone with a 1080x2400p resolution display. The Qualcomm SM7125 Snapdragon 720G chipset is paired with 4/6GB of RAM and 64/128GB of storage. The main camera is 48+8+5+2MP and the selfie camera is 16MP. The battery has a 5000mAh capacity.`,
-    image:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    image:pic,
     author:"Vinoth"
 },
 ,{
